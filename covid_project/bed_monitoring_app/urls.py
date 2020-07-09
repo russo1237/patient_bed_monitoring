1bdd9311b5c2e4c3eba24b026e3a0a861a01ccab
russo1237 <russo1237@yahoo.com>
2020-07-09 19:08:09 +0530

    Added templates for hospital user

--- covid_project/bed_monitoring_app/urls.py
+++ covid_project/bed_monitoring_app/urls.py
@@ -10,7 +10,9 @@ urlpatterns = [
     path('register/', views.register, name = 'register'),
     path('user_login/', views.user_login, name = 'user_login'),
     path('about_us/', views.about_us, name = 'about_us'),
-    path('update_details/', views.update_details, name ='update_details'),
+    path('update_bed_details/', views.update_bed_details, name ='update_bed_details'),
+    path('update_govt_beds/', views.update_govt_beds, name ='update_govt_beds'),
+    path('update_hos_beds/', views.update_hos_beds, name ='update_hos_beds'),
     path('logout/', views.logout_view, name='logout'),
     path('first_page/', views.page_after_login, name = 'first_page'),
 
